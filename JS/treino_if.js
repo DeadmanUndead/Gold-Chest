d1ffec07b7b29564f4892befd68f54b8f95bf13d
DeadmanUndead <guillermomedlam@hotmail.com.br>
2022-03-10 12:02:40 -0300

    Treino_Html&CSS

---
+++ JS/treino_if.js
@@ -0,0 +1,61 @@
+/*
+////////////////////////////////////////////////////////////////
+TESTE COM IF
+var name, IsHuman 
+
+name = "Vlad"
+
+let age
+
+age = 300
+
+if (age < 100) {
+  IsHuman = true
+  console.log(IsHuman)
+}else if (age > 100){
+  IsHuman = false
+  console.log(IsHuman)
+}
+////////////////////////////////////////////////////////////////
+TESTE FUNÇÃO (ESTUDAR MAIS)
+function Person(nome) {
+	this.nome = nome
+  this.age = 34
+}
+const mayk = new Person("Mayk") 
+////////////////////////////////////////////////////////////////
+TESTE EM TRANSFORMAÇÃO DE STRING
+let number1, number2
+
+number1 = 23123
+
+number2 = 42
+
+if (String(number1).length > 6) {
+  console.log("fish")
+}else if (String(number2).length > 1) {
+  console.log(number1 + number2)
+} 
+////////////////////////////////////////////////////////////////
+TESTE EM COMO O .SPLIT TRANSFORMA EM ARRAY
+let frase = "eu amor chocolate"
+
+let array = frase.split(" ")
+
+console.log(array[2]) 
+////////////////////////////////////////////////////////////////
+CRIANDO OBJETOS DENTRO DE UM OBJETO
+const person = {
+  1: {
+  name: "mayk",
+  age: 23,
+},
+2: {
+  name: "gui",
+  age: 34,
+},
+}
+
+console.log(person)
+*/
+
